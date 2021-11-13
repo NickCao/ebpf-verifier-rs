6af4ca93fe0cca616bd917c479d16e216291689f
Nick Cao <nickcao@nichi.co>
2021-11-13 12:12:16 +0800

    remove hacks on include path

--- build.rs
+++ build.rs
@@ -1,11 +1,8 @@
 fn main() {
-    autocxx_build::Builder::new(
-        "src/lib.rs",
-        &["src/", &std::env::var("AUTOCXX_INC").unwrap()],
-    )
-    .extra_clang_args(&["-std=c++17"])
-    .expect_build()
-    .compile("verifier");
+    autocxx_build::Builder::new("src/lib.rs", &["src/"])
+        .extra_clang_args(&["-std=c++17"])
+        .expect_build()
+        .compile("verifier");
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/verifier.hpp");
 }
